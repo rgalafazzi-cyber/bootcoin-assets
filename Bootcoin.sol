58c7b3355b2e7a697236f13cf5641f6c0c5ad1c1
Boot Coin <rgalafazzi@gmail.com>
2026-03-19 14:27:15 -0300

    Create Bootcoin.sol

---
+++ Bootcoin.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract Bootcoin is ERC20, Ownable {
+
+    constructor(uint256 initialSupply) 
+        ERC20("Bootcoin", "BOOT") 
+        Ownable(msg.sender)
+    {
+        _mint(msg.sender, initialSupply * 10 ** decimals());
+    }
+
+    function rewardUser(address user, uint256 amount) public onlyOwner {
+        _mint(user, amount * 10 ** decimals());
+    }
+
+    function burn(uint256 amount) public {
+        _burn(msg.sender, amount * 10 ** decimals());
+    }
+}
